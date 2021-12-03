db3c6cca1540f61142f9686d931b2966984603e8
Sebastian Knoll (TUG) <sebastian.knoll@tugraz.at>
2021-12-03 14:54:13 +0100

    Basic Dashboard with process

--- DoE/Dashboard/Layout.py
+++ DoE/Dashboard/Layout.py
@@ -1,41 +1,45 @@
 from dash import html, Dash
 import plotly.express as px
+from datetime import datetime
 
-class Layout():
+def getDoELayout():
 
-    def __init__(self) -> None:
-        pass
-
-    def getLayout(self):
-        return html.Div(
-            children=[
-                html.H1(
-                    style={
-                        'textAlign': 'center'
-                    },
-                    children="Base Layout"
-                )
-            ]
-        )
-
-    def appendExternalCSSFiles(self, dashboard:Dash):
-        pass # dashboard.css.append_css({"external_url": ""})
-
-class DoEDashboard(Layout):
-
-    def appendExternalCSSFiles(self, dashboard: Dash):
-        dashboard.css.append_css({"external_url": "./Assets/main.css"})
-
-    def getLayout(self):
-
-        return html.Div(
-            id="content",
-            children=[
-                html.H1(children="DoE Dashboard"),
-                html.Div(children=[
-                    html.P(style={'fontWeight': 'bold'},children="STATE:"),
-                    html.P(children="waiting")
+    return html.Div(
+        id="content",
+        children=[
+            html.H1(
+                children="DoE Dashboard"
+            ),
+            html.Div(
+                id="processControl",
+                children=[
+                    html.P(children="STATE: "),
+                    html.P(id="state", style={'fontWeight': 'bold'}, children="READY"),
+                    html.Br(), 
+                    html.Progress(id="stateProgessBar"),
+                    html.Div(
+                        id="buttonContainer",
+                        children=[
+                            html.Button(id="buttonStart", children="Let's do some DoE"),
+                            html.Button(id="buttonStop", children="Coffe break - now"),
+                        ]
+                    )
                 ]
-                )
-            ]
-        )
+            ),
+            html.Div(
+                id="footer",
+                children=[
+                    html.A(children="Made with Plotly", href="https://plotly.com/", target="_blank"),
+                    html.A(children="RCPE", href="https://www.rcpe.at/", target="_blank"),
+                    html.A(children="Sebastian Knoll", href="https://sebastianknoll.net/me", target="_blank")
+                ]
+            ),
+            html.Div(
+                id="header",
+                children=[
+                    html.P(children="System Time:"),
+                    html.P(id="systemTime", children=datetime.now().strftime("%d.%m.%Y - %H:%M:%S"))
+                ]
+            )
+        ]
+    )
--- DoE/Dashboard/Server.py
+++ DoE/Dashboard/Server.py
@@ -7,23 +7,57 @@ from dash import dcc
 from dash import html
 import plotly.express as px
 import pandas as pd
+from dash.dependencies import Input, Output
+from dash.long_callback import DiskcacheLongCallbackManager
 
+import time
 import Layout
+import diskcache
 
+cache = diskcache.Cache("./cache")
+long_callback_manager = DiskcacheLongCallbackManager(cache)
+
+dashboard = dash.Dash(__name__, long_callback_manager=long_callback_manager)
 
 def startServer(debug=True):
 
-    dashboard = dash.Dash(__name__)
     dashboard.title = "Automated DoE"
-    layout = Layout.DoEDashboard()
-    #self.dashboard._favicon
-    #self.layout.appendExternalCSSFiles(self.dashboard)
 
-    dashboard.layout = layout.getLayout()
+    #dashboard._favicon
+    #layout.appendExternalCSSFiles(self.dashboard)
 
-    print(dashboard.get_asset_url('main.css'))
+    dashboard.layout = Layout.getDoELayout()
 
     dashboard.run_server(debug=debug)
+
+
+@dashboard.long_callback(
+    output=Output("state", "children"),
+    inputs=Input("buttonStart", "n_clicks"),
+    running=[
+        (Output("buttonStart", "disabled"), True, False),
+        (Output("buttonStop", "disabled"), False, True),
+        (
+            Output("state", "style"),
+            {"visibility": "hidden"},
+            {"visibility": "visible"},
+        ),
+        (
+            Output("stateProgessBar", "style"),
+            {"visibility": "visible"},
+            {"visibility": "hidden"},
+        ),
+    ],
+    cancel=[Input("buttonStop", "n_clicks")],
+    progress=[Output("stateProgessBar", "value"), Output("stateProgessBar", "max")],
+)
+def callback(set_progress, n_clicks):
+    total = 10
+    for i in range(total):
+        time.sleep(0.5)
+        set_progress((str(i + 1), str(total)))
+    return [f"Clicked {n_clicks} times"]
+
 
 
 if __name__ == "__main__":
