4b6bfb18976a23d14a0225bafe59bfd3af648dcf
Sebastian Knoll (TUG) <sebastian.knoll@tugraz.at>
2022-03-01 12:21:05 +0100

    Fixed import-Bug

--- DoE/Server/main.py
+++ DoE/Server/main.py
@@ -273,9 +273,6 @@ class Server(http.server.SimpleHTTPRequestHandler):
         
         if "import" in self.path:
 
-            xamControl.resetImport()
-            ImportExport.deleteCurrentImportFile()
-
             infos = ImportExport.importInfos()
 
             if not infos["isAvailable"]:
